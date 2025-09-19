2cfd31fc64bc38b5b60807b45cb86c348ac9be85
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-19 16:04:56 -0300

    Agregamos una carpeta con las variables e objetos y una carpeta con types

---
+++ src/consts/index.ts
@@ -0,0 +1,33 @@
+import type { NavigationState } from '@/types/index.types'
+
+//* La navegación
+const navigation: NavigationState = [
+  {
+    name: 'Inicio',
+    href: '/',
+  },
+  {
+    name: 'Nosotros',
+    href: '/nosotros/',
+  },
+  {
+    name: 'Blog',
+    href: '/blog/',
+  },
+  {
+    name: 'Proceso',
+    href: '/proceso/',
+  },
+  {
+    name: 'Menú',
+    href: '/menu/',
+  },
+  {
+    name: 'Galería',
+    href: '/galeria/',
+  },
+  {
+    name: 'Contacto',
+    href: '/contacto/',
+  },
+]
