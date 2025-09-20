d5698102e29eb4a8c711951794fbbf82a3831539
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-20 13:57:42 -0300

    Agregué el fetch para traerme los datos de la página Nosotros y también el schema de validación con Zod

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,1 +1,16 @@
+import { z } from "astro:content";
+
+const ContentRendered = z.object({
+  rendered: z.string()
+})
+
+export const BaseWPSchema = z.object({
+  id: z.number(),
+  title: ContentRendered,
+  content: ContentRendered,
+  acf: z.object({
+    subtitle: z.string()
+  })
+})
+
 export type NavigationState = { name: string; href: string }[]
