2a11b3fb42f267817730434e14f604ddadf79bcb
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-20 17:55:07 -0300

    Schema que acepta las imágenes

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,16 +1,31 @@
 import { z } from "astro:content";
 
-const ContentRendered = z.object({
+const contentRenderedSchema = z.object({
   rendered: z.string()
+})
+
+const imageSchema = z.object({
+  url: z.string(),
+  width: z.number(),
+  height: z.number(),
+})
+
+const featuredImagesSchema = z.object({
+  thumbnail: imageSchema,
+  medium: imageSchema,
+  medium_large: imageSchema,
+  large: imageSchema,
+  full: imageSchema,
 })
 
 export const BaseWPSchema = z.object({
   id: z.number(),
-  title: ContentRendered,
-  content: ContentRendered,
+  title: contentRenderedSchema,
+  content: contentRenderedSchema,
   acf: z.object({
     subtitle: z.string()
-  })
+  }),
+  featured_images: featuredImagesSchema,
 })
 
 export type NavigationState = { name: string; href: string }[]
